af7bb79f7081becaa7db4192ea5ee63e2f56e9d1
Philipp Nitsche <s0573651@htw-berlin.de>
2021-01-14 22:07:40 +0100

    Minim File Handler to initialize Minim Object

---
+++ src/main/java/MinimFileSystemHandler.java
@@ -0,0 +1,18 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class MinimFileSystemHandler {
+	 public String sketchPath( String fileName ) {
+		 return System.getProperty("user.dir") + fileName;
+	 }
+	 
+	 public InputStream createInput( String fileName ) {
+		 try {
+			return new FileInputStream(fileName);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			return null;
+		}
+	 }
+}
