8853879c0420adad6bdcf20bd637a6059e9e800c
soosaasees <nachokaese@gmx.net>
2021-01-14 15:56:11 +0100

    AmpAnalyzer spielt Samples bei Start und Ende eines Songs ab. Schwellwerte sind angepasst.

--- src/main/java/AmplitudeAnalyzer.java
+++ src/main/java/AmplitudeAnalyzer.java
@@ -6,21 +6,26 @@ public class AmplitudeAnalyzer extends PApplet implements Runnable  {
     private Amplitude amplitude=new Amplitude(this);
     private AudioIn stream;
     private boolean SongInProgress=false;
+    private SamplePlayer sPlayer;
     
     public AmplitudeAnalyzer(AudioIn in, SamplePlayer player)  {
         stream=in;
+        sPlayer=player;
     }
 
     private void checkSongInProgress() throws InterruptedException {
         if(SongInProgress)  {
-            if(amplitude.analyze()==0)  {
+            if(amplitude.analyze()<=0.2)  {
                 wait(1000);
-                if(amplitude.analyze()==0)  {
+                if(amplitude.analyze()<=0.2)  {
                     SongInProgress=false;
+                    sPlayer.playCheering();
                 }
             }
-        } else if(amplitude.analyze()>0)
+        } else if(amplitude.analyze()>=0.2)    {
             SongInProgress=true;
+            sPlayer.playCheering();
+        }
     }
     
     public boolean isSongInProgress() {
