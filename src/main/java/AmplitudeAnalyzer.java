0c9989d4df850d151dd6aff07300b5992e40398e
soosaasees <nachokaese@gmx.net>
2021-01-15 15:10:43 +0100

    lol

--- src/main/java/AmplitudeAnalyzer.java
+++ src/main/java/AmplitudeAnalyzer.java
@@ -3,22 +3,44 @@ import processing.sound.*;
 
 public class AmplitudeAnalyzer extends PApplet implements Runnable  {
 
-    private Amplitude amplitude=new Amplitude(this);
-    private AudioIn stream;
+    private final Amplitude amplitude=new Amplitude(this);
+    private final AudioIn stream;
     private boolean SongInProgress=false;
-    private SamplePlayer sPlayer;
+    private final SamplePlayer sPlayer;
+    private boolean blocker=false;//ist true, wenn checkSongInProgress am machen ist und verhindert andere aufrufe
+    boolean ausTimer;
     
     public AmplitudeAnalyzer(AudioIn in, SamplePlayer player)  {
         stream=in;
         sPlayer=player;
     }
 
+    /**
+     * checkt, ob sich der Status des Songs (an, aus) geändert hat und spielt jubeln falls ja. beendet den block, wenn durch
+     * @throws InterruptedException
+     */
     private void checkSongInProgress() throws InterruptedException {
-        if(SongInProgress)  {
-            if(amplitude.analyze()<=0.2)  {
-                Thread.sleep(1000);
-                if(amplitude.analyze()<=0.2)  {
-                    SongInProgress=false;
+        if(SongInProgress) {
+            if (amplitude.analyze() <= 0.2) {
+                ausTimer = true;
+                Thread t = new Thread(() -> {
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    ausTimer = false;
+                });
+                t.start();
+                boolean ausCheck = true; //ist true, wenn es kein Anzeichen gibt, dass der Song weitergeht
+                while (ausTimer) {
+                    Thread.sleep(1);
+                    if (amplitude.analyze() >= 0.2) {
+                        ausCheck = false;
+                    }
+                }
+                if (ausCheck)   {
+                    SongInProgress = false;
                     sPlayer.playCheering();
                 }
             }
@@ -26,6 +48,7 @@ public class AmplitudeAnalyzer extends PApplet implements Runnable  {
             SongInProgress=true;
             sPlayer.playCheering();
         }
+        blocker=false;
     }
     
     public boolean isSongInProgress() {
@@ -40,7 +63,10 @@ public class AmplitudeAnalyzer extends PApplet implements Runnable  {
             SongInProgress=true;
         while(!Thread.interrupted()) {
             try {
-                checkSongInProgress();
+                if(!blocker)    {
+                    blocker=true;
+                    checkSongInProgress();
+                }
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
